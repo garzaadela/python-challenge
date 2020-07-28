a48d2ca468ff18a757a75294010066c704ff84b0
Adela Garza <a.garza045@gmail.com>
2020-07-28 00:10:49 -0500

    added second/third question

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,5 +5,20 @@ budget_df = pd.read_csv("Resources/budget_data.csv")
 count = budget_df["Date"].value_counts()
 count = count.sum()
 print("Financial Analysis")
-print("-----------------------------")
-print(f"Total: {count}")
+print("-----------------------")
+print(f"Total Months: {count}")
+# total amount of "Profit/Losses"
+Total = budget_df["Profit/Losses"].sum()
+print(f"Total: (${Total})")
+# Average Change
+Average = budget_df["Profit/Losses"].diff().mean()
+print(f"Average Change: (${Average})")
+#Max Increase in Profits
+#diff_df = budget_df["Profit/Losses"].diff()
+#diff_df.max()
+#max_value = diff_df.max()
+#max_val_index = diff_df.loc[diff_df["Profit/Losses"] == max_value].index[0]
+#max_val_date = budget_df.iloc[max_val_index].Date
+#print(f"Greatest Increase in Profits: {max_val_date} (${max_value})")
+
+
